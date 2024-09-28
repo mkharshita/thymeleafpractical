e17bc55ae6a9c7773dde984b023380e7b9392383
mkharshita <mkharshita5@gmail.com>
2024-09-28 23:49:53 +0530

    iteration

---
+++ README.md
@@ -0,0 +1,4 @@
+for testing use:
+
+http://localhost:8080/about
+http://localhost:8080/example-loop
--- src/main/java/com/practice/thymeleafpractical/controller/MyController.java
+++ src/main/java/com/practice/thymeleafpractical/controller/MyController.java
@@ -1,11 +1,15 @@
 package com.practice.thymeleafpractical.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 
 @Controller
@@ -17,4 +21,12 @@ public class MyController {
         model.addAttribute("currentDate", new Date().toString());
         return "about";
     }
+    
+    @GetMapping("/example-loop")
+    public String iterateHandler(Model model) {
+        List<String> names = List.of("Harshita","Shruti","Akshat");
+        model.addAttribute("names", names);
+        return "iterate";
+    }
+    
 }
