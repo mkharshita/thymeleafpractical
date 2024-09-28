80c25577327b4cd4e87a7d870f567ef2e4c4e23c
mkharshita <mkharshita5@gmail.com>
2024-09-28 23:29:40 +0530

    initial commit

---
+++ src/main/java/com/practice/thymeleafpractical/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.practice.thymeleafpractical.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/about", method=RequestMethod.GET)
+    public String about(Model model){
+        model.addAttribute("name", "Harshita");
+        model.addAttribute("currentDate", new Date().toString());
+        return "about";
+    }
+}
